ff20d1b4d80edf3019870e0ee28d71fc39541808
Bryn Knowles <bryn.knowles@gmail.com>
2020-07-06 17:42:22 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -4,13 +4,13 @@ def loop_message_five_times(message)
   5.times {puts message}
 end
 
-def loop_message_n_times(message, number)
-  10.times { |i| puts "hello #{i}" }
-end 
+# def loop_message_n_times(message, number)
   
-# def output_array
+# end 
   
-# end
+def output_array
+  
+end
     
 # def return_string_array
   
